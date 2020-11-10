e3f8bd34bec3b7972662b9c34893f5154ba69f38
Marcelo Marques Lima <marcelomarqueslima.dev@gmail.com>
2020-11-10 15:55:33 -0300

    Organização de Layout
    Estruturando layout da escopo do projeto

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 
 require_once 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
+use Slim\Slim;
+
+use MsCore\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function(){
-    //echo 'OK';
     
-    $sql = new MsCore\DB\Sql();
-
-    $results = $sql->select("SELECT * FROM tb_users");
-
-    echo json_encode($results);
+    $page = new Page();
+    
+    $page->setTpl("index");
     
 });
 
